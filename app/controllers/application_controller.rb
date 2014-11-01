463dcbb6b51ba798d2decaa7625e2542dff150df
Arturo Diaz <arturo.diaz@wavesoluciones.com>
2014-11-01 13:24:36 -0600

    check saldo

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,5 +7,12 @@ class ApplicationController < ActionController::Base
     @current_org ||= Organization.find(session[:organization_id]) if session[:organization_id]
   end
 
+  def client
+    account_sid = 'ACbb49022bd031a99d9967595de4d4216a'
+    auth_token = '5be587638d9fa909c1ab0f116974dc65'
+    @client = Twilio::REST::Client.new account_sid, auth_token
+  end
+
+
   helper_method :current_org
 end
--- app/controllers/users_controllers.rb
+++ app/controllers/users_controllers.rb
@@ -1,32 +1,50 @@
 class UsersController < ApplicationController 
-  def create
+  def endpoint
     phone = params['From']
     body = params['Body']
 
-    
-    Compropago.api_key = "sk_test_8305a01416373e4b4"
-
+    # Usuario deposita dinero
     if body.match('depositar')[0]
-      amount = /(\d+)/.match(body)[0]
-      phone = phone.gsub('+52','')
 
-      puts amount
-      puts phone
-
-      charge = Compropago::Charge.create({ product_price: amount,
-                                           product_name: "Deposito a #{phone}",
-                                           customer_name: "SMS Pay",
-                                           customer_email: "noreply@compropago.com",
-                                           payment_type: "OXXO"})
-      puts charge
-
-      Compropago::SMS.create(payment_id: charge.payment_id, customer_phone: phone, customer_company_phone: "MOVISTAR")
-
-      if charge
-        User.create(phone: phone)
+      Compropago.api_key = "sk_test_8305a01416373e4b4"
+  
+      if body.match('depositar')[0]
+        amount = /(\d+)/.match(body)[0]
+        phone = phone.gsub('+52','')
+  
+        charge = Compropago::Charge.create({ product_price: amount,
+                                             product_name: "Deposito a #{phone}",
+                                             customer_name: "SMS Pay",
+                                             customer_email: "noreply@compropago.com",
+                                             payment_type: "OXXO"})
+  
+        Compropago::SMS.create(payment_id: charge.payment_id, customer_phone: phone, customer_company_phone: "MOVISTAR")
+  
+        if charge
+          User.create(phone: phone, balance: amount.to_i)
+        end
+  
+        respond_to json: nil, status: 200
       end
 
-      render nil
+    # Usuario solicita su saldo 
+    elsif body.match('saldo')[0]
+      user = User.find_by_phone(phone: phone.gsub('+52',''))
+      client.messages.create(
+        from: "+15209993914",
+        to: "00521" + phone,
+        body: "Tu saldo disponible es de: #{user.balance}"
+      )
+
+      respond_to json: nil, status: 200
     end
   end
+
+  def check_balance
+    if params['api'] == "true"
+      phone = params['From']
+      body = params['Body']
+
+
+
 end
