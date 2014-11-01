2489196fcd4b79aabab68eebae5db8730bd3634e
Arturo Diaz <arturo.diaz@wavesoluciones.com>
2014-11-01 15:31:47 -0600

    fix

--- app/controllers/users_controllers.rb
+++ app/controllers/users_controllers.rb
@@ -4,31 +4,30 @@ class UsersController < ApplicationController
     body = params['Body']
 
     # Usuario deposita dinero
-    if body.match('depositar')[0]
+    if body.match('depositar')
 
       Compropago.api_key = "sk_test_8305a01416373e4b4"
   
-      if body.match('depositar')[0]
-        amount = /(\d+)/.match(body)[0]
-        phone = phone.gsub('+52','')
+      amount = /(\d+)/.match(body)[0]
+      phone = phone.gsub('+52','')
   
-        charge = Compropago::Charge.create({ product_price: amount,
-                                             product_name: "Deposito a #{phone}",
-                                             customer_name: "SMS Pay",
-                                             customer_email: "noreply@compropago.com",
-                                             payment_type: "OXXO"})
+      charge = Compropago::Charge.create({ product_price: amount,
+                                           product_name: "Deposito a #{phone}",
+                                           customer_name: "SMS Pay",
+                                           customer_email: "noreply@compropago.com",
+                                           payment_type: "OXXO"})
   
-        Compropago::SMS.create(payment_id: charge.payment_id, customer_phone: phone, customer_company_phone: "MOVISTAR")
+      Compropago::SMS.create(payment_id: charge.payment_id, customer_phone: phone, customer_company_phone: "MOVISTAR")
   
-        if charge
-          User.create(phone: phone, balance: amount.to_i)
-        end
-  
-        respond_to json: nil, status: 200
+      if charge
+        User.create(phone: phone, balance: amount.to_i)
       end
+  
+      respond_to json: nil, status: 200
+    end
 
     # Usuario solicita su saldo 
-    elsif body.match('saldo')[0]
+    elsif body.match('saldo')
       user = User.find_by_phone(phone: phone.gsub('+52',''))
       client.messages.create(
         from: "+15209993914",
